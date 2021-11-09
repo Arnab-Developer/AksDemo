b2761671caba439dceaf2e2bfb6e863d472699ec
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2021-11-09 11:56:40 +0530

    api2

---
+++ AksDemo.Api2/Controllers/HelloApiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AksDemo.Api2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloApiController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get(string name) => $"Hello {name}, this is API 2";
+    }
+}
